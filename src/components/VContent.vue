ef323e1bb3089cb39165175ae6a90810558b6eb0
zerescas <zerescas@gmail.com>
2024-08-28 14:45:09 +0900

    implement VContent

---
+++ src/components/VContent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="vcontent">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+.vcontent {
+  display: flex;
+  margin: auto;
+  max-width: $content-max-width;
+  overflow: hidden;
+  padding: 0 $content-x-padding;
+  width: 100%;
+}
+</style>
