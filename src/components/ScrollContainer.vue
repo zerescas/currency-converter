a0dba3589ae2a9d1cd4d06cc0ad6921779bebcd4
zerescas <zerescas@gmail.com>
2024-08-28 14:45:29 +0900

    implement ScrollContainer

---
+++ src/components/ScrollContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="scroll-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.scroll-container {
+  margin: auto;
+  max-height: 100%;
+  overflow: auto;
+  width: 100%;
+}
+</style>
